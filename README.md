bd57cb8eec919bd35bde43faa5d76c25124e012e
nirberko <nirberko@gmail.com>
2020-06-18 18:41:55 +0300

    readme

---
+++ README.md
@@ -0,0 +1,6 @@
+[![lerna](https://img.shields.io/badge/maintained%20with-lerna-cc00ff.svg)](https://lerna.js.org/)
+
+## How to run this project
+
+ 1. `npm run bootstrap` to install all dependencies by Lerna
+ 2.   `npm start` to start both server and client
--- packages/client/src/routes/index.tsx
+++ packages/client/src/routes/index.tsx
@@ -4,13 +4,11 @@ import { Route, Switch } from "react-router-dom";
 import { Template }  from "../components/Template";
 
 import { Dashboard } from "./Dashboard";
-import { Page }      from "./Page";
 
 export const Routes = () => (
     <Template>
         <Switch>
             <Route path='/' exact component={Dashboard} />
-            <Route path='/page' component={Page} />
         </Switch>
     </Template>
 );
